ab2ceccf818e0a3a6f9503a33cc4410c164eb4d6
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-23 22:54:09 +0100

    feat: add title to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
 
 const NotFound = () => (
   <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>OH NO - 404</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <Layout>
       <h1>Not Found</h1>
     </Layout>
