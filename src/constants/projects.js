f129f04dd5d80342c98c321af9ca635f045321a2
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-23 22:15:34 +0100

    chore: add sample projects page

---
+++ src/constants/projects.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    image: 'https://www.placecage.com/300/300',
+    title: 'Bombay Train Map',
+    blurb: 'I made a map for the Bombay train system',
+    description:
+      "Love, exciting and new. Come aboard, we're expecting you. Love, life's sweetest reward. Let it flow, it floats back to you. The Love Boat soon will be making another run. The Love Boat promises something for everyone. Set a course for adventure, your mind on a new romance. Love won't hurt anymore. It's an open smile on a friendly shore. Yes, love! It's love!",
+    links: {
+      github: 'https://github.com/nkhil',
+      liveLink: 'https://google.com',
+    },
+    tags: ['Design'],
+  },
+  {
+    image: 'https://www.placecage.com/300/300',
+    title: 'Brexit means Breadsticks',
+    blurb:
+      "A Chrome extension that turns the word 'brexit' into 'breadsticks' on all web pages.",
+    description:
+      "Love, exciting and new. Come aboard, we're expecting you. Love, life's sweetest reward. Let it flow, it floats back to you. The Love Boat soon will be making another run. The Love Boat promises something for everyone. Set a course for adventure, your mind on a new romance. Love won't hurt anymore. It's an open smile on a friendly shore. Yes, love! It's love!",
+    links: {
+      github: 'https://github.com/nkhil',
+      liveLink: 'https://google.com',
+    },
+    tags: ['JavaScript'],
+  },
+  {
+    image: 'https://www.placecage.com/300/300',
+    title: 'UK Bank Holidays',
+    blurb: 'A fully tested React app that displays',
+    description:
+      "Love, exciting and new. Come aboard, we're expecting you. Love, life's sweetest reward. Let it flow, it floats back to you. The Love Boat soon will be making another run. The Love Boat promises something for everyone. Set a course for adventure, your mind on a new romance. Love won't hurt anymore. It's an open smile on a friendly shore. Yes, love! It's love!",
+    links: {
+      github: 'https://github.com/nkhil',
+      liveLink: 'https://google.com',
+    },
+    tags: ['JavaScript', 'React'],
+  },
+  {
+    image: 'https://www.placecage.com/300/300',
+    title: 'Pineapple Journal',
+    blurb: 'A full-stack journaling app',
+    description:
+      "Love, exciting and new. Come aboard, we're expecting you. Love, life's sweetest reward. Let it flow, it floats back to you. The Love Boat soon will be making another run. The Love Boat promises something for everyone. Set a course for adventure, your mind on a new romance. Love won't hurt anymore. It's an open smile on a friendly shore. Yes, love! It's love!",
+    links: {
+      github: 'https://github.com/nkhil',
+      liveLink: 'https://google.com',
+    },
+    tags: ['JavaScript', 'React'],
+  },
+];
