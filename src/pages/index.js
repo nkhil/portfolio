b24860dec06392583ea42e4dca9b2c39017b0142
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-23 22:13:46 +0100

    feat: use helmet to add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 /* eslint-disable react/display-name */
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 
 export default () => (
   <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Nikhil Vijayan - Hi, I'm Nikhil.</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <Layout>
       <Hero />
     </Layout>
