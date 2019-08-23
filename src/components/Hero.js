ef45eb863079ccc9af01971717227e7dbd85f8e7
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-23 22:54:20 +0100

    feat: add styling to Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,27 @@ import Container from './styled_components/Container';
 import Flex from './styled_components/Flex';
 import image from '../../static/images/image.png';
 
-const HeroContainer = styled(Container)`
+const HeroContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:700|Raleway&display=swap');
+
   min-height: 60vh;
   border: 1px solid blue;
-  color: black;
+  color: white;
   padding: 0;
+  background-color: #005aef;
+
+  h1 {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 4rem;
+    margin: 1.5rem 0 0 0;
+  }
+
+  p {
+    font-family: 'Raleway', sans-serif;
+    font-size: 1.5rem;
+    text-align: center;
+    margin: 10px;
+  }
 `;
 
 const StyledImg = styled.img`
@@ -17,11 +33,12 @@ const StyledImg = styled.img`
   flex: 1;
   align-self: center;
   width: 150px;
+  margin-top: 2rem;
 `;
 
 const Hero = () => (
   <HeroContainer>
-    <Container fullVertical full>
+    <Container full>
       <Flex column alignCenter>
         <StyledImg src={image} alt="Nikhil" />
         <h1>Hi, I'm Nikhil</h1>
