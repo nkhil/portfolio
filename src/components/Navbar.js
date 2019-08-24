e636b65f991623681ddfb1bae8b432a63def861e
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-24 19:09:24 +0100

    chore: add styledImg to NavBar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,46 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import NavItems from '../constants/links';
 import Flex from './styled_components/Flex';
 import Container from './styled_components/Container';
+import Logo from '../../static/images/logo.png';
+
+const StyledImg = styled.img`
+  src: url(${props => props.src});
+  alt: (${props => props.alt});
+  align-self: center;
+  flex: 1;
+  max-width: 100%;
+  height: auto;
+  margin-top: ${props => {
+    if (props.margin) return props.margin;
+    return '0px';
+  }};
+`;
+
+const Div = styled.div`
+  display: flex;
+  margin-top: 15px;
+  margin-left: 50px;
+  margin-right: auto;
+`;
 
 export default function Navbar({ className }) {
   return (
     <Container full fullVertical className={className}>
       <Flex justifyEnd>
-        <Flex justifyEnd>
+        <Div>
+          <Link to='/'>
+            <StyledImg
+              src={Logo}
+              alt="Nikhil"
+              style={{ alignContent: 'flex-start' }}
+            />
+          </Link>
+        </Div>
+        <Flex justifyCenter>
           <ul>
             {NavItems.map((item, i) => (
               <li key={i}>
