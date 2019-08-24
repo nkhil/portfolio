d2544e9864adb04fbd9f078e6f38f79a536ba0ce
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-24 11:14:52 +0100

    chore: Make container min width 900px

--- src/components/styled_components/Container.js
+++ src/components/styled_components/Container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   max-width: ${props => {
     if (props.width) return props.width;
-    return '700px';
+    return '900px';
   }};
 
   margin: auto;
