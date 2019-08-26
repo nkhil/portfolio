bc6fb7eea6506cacdd0f4a4870ae1dfad661b67f
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-26 13:22:21 +0100

    chore: modify buton styling to have appropriate margin

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import Container from './styled_components/Container';
 import Flex from './styled_components/Flex';
 import image from '../../static/images/image.png';
 import Colours from '../constants/colours';
 import MainContainer from './styled_components/MainContainer';
 import StyledImg from './styled_components/StyledImg';
+
+const Button = styled.button`
+  font-size: 1em;
+  border-radius: 3px;
+
+  && {
+    margin: 15px 10px;
+  }
+`;
 
 const Hero = () => (
   <MainContainer bgColor={Colours.primaryBlue}>
@@ -13,15 +23,15 @@ const Hero = () => (
         <StyledImg src={image} alt="Nikhil" />
         <h1>Hi, I'm Nikhil.</h1>
         <p>I really like making things and solving interesting problems.</p>
-        <Flex>
+        <Flex justifyCenter>
           <a href="https://www.github.com/nkhil" rel="noopener norefferer" target="_blank">
-            <button type="button">My Github</button>
+            <Button type="button">My Github</Button>
           </a>
           <a href="https://registry.jsonresume.org/nkhil" target="_blank" rel="noopener norefferer">
-            <button type="button">Download My CV</button>
+            <Button>Download My CV</Button>
           </a>
           <a href="https://www.linkedin.com/in/nikhilvijayan/" target="_blank" rel="noopener norefferer">
-            <button type="button">My LinkedIn</button>
+            <Button type="button">My LinkedIn</Button>
           </a>
         </Flex>
       </Flex>
