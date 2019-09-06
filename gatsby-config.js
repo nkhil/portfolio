0ccf3628605184bb2aaff98924542c7769956562
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-06 21:50:19 +0100

    chore: gatsby-config.js update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,23 @@
 
 module.exports = {
   /* Your site config here */
-}
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+};
