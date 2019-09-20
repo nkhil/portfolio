3da4fea4f053f4fb633a3fe45b13658141df5386
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-20 15:34:56 +0530

    feat: modify Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 import NavItems from '../constants/links';
 import Flex from './styled_components/Flex';
 import Container from './styled_components/Container';
 import Logo from '../../static/images/logo.png';
+import Colours from '../constants/colours';
 
 const StyledImg = styled.img`
   src: url(${props => props.src});
   alt: (${props => props.alt});
-  align-self: center;
   flex: 1;
   max-width: 100%;
   height: auto;
@@ -18,33 +19,39 @@ const StyledImg = styled.img`
     if (props.margin) return props.margin;
     return '0px';
   }};
+  margin-left: 100px;
 
   @media screen and (max-width: 599px) {
     display: none;
   }
 `;
 
-const Div = styled.div`
-  display: flex;
-  margin-top: 15px;
-  margin-left: 50px;
-  margin-right: auto;
+const StyledArrow = styled(MdKeyboardArrowRight)`
+  display: none;
+
+  @media screen and (max-width: 599px) {
+    display: inline;
+    justify-items: flex-end;
+  }
 `;
 
-export default function Navbar({ className }) {
+export default function Navbar({ className, backgroundColor }) {
   return (
-    <Container full fullVertical className={className}>
+    <Container full fullVertical backgroundColor={backgroundColor} className={className}>
       <Flex justifyEnd>
-        <Div>
+        <Flex style={{ marginRight: 'auto' }}>
           <Link to="/">
-            <StyledImg src={Logo} alt="Nikhil" style={{ alignContent: 'flex-start' }} />
+            <StyledImg src={Logo} alt="Nikhil" style={{ alignContent: 'flex-start' }} margin="15px" />
           </Link>
-        </Div>
-        <Flex justifyCenter fillAvailable>
+        </Flex>
+        <Flex fillAvailable>
           <ul>
             {NavItems.map((item, i) => (
               <li key={i}>
-                <Link to={item.path}>{item.name}</Link>
+                <Link to={item.path}>
+                  {item.name}
+                  <StyledArrow />
+                </Link>
               </li>
             ))}
           </ul>
