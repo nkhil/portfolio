aa8c1e42f0067ea40c494f8c0e3a101805cf3155
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-21 16:53:09 +0530

    refactor: BlogPost component

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,26 +4,41 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 import Container from './styled_components/Container';
 import MainContainer from './styled_components/MainContainer';
+import GridContainer from './styled_components/GridContainer';
+import GridItem from './styled_components/GridItem';
 
-function BlogPost({ className, data }) {
+const H2 = styled.h2`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 3rem;
+  text-align: center;
+  color: #2e2e2e;
+  margin: 0;
+  padding: 50px 10px 0px;
+`;
+
+const P = styled.p`
+  && {
+    font-size: 1.2rem;
+    font-family: 'Montserrat', sans-serif;
+    color: #2e2e2e;
+  }
+`;
+
+function BlogPost({ data }) {
   return (
     <>
-      <div className={className}>
+      <div>
         <MainContainer bgColor="#eee">
           <Container full backgroundColor="#eee" fullVertical>
-            <h2>Writing</h2>
-            <p>
+            <H2>Writing</H2>
+            <P>
               I learn best by teaching. It also helps me document my learning. Here are a few articles I've written.
-            </p>
-            <ul>
-              {data.map(({ node }) => (
-                <li key={node.id}>
-                  <Link to={node.frontmatter.path}>
-                    <span className="test">{node.frontmatter.title}</span>
-                  </Link>
-                </li>
+            </P>
+            <GridContainer>
+              {data.map((project, index) => (
+                <GridItem index={index} project={project} key={index} style={{ listStyleType: 'none' }} />
               ))}
-            </ul>
+            </GridContainer>
           </Container>
         </MainContainer>
       </div>
