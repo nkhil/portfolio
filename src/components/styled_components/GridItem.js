775025fa4efa8b744f74f3932a9a3fd72474d961
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-21 16:55:14 +0530

    chore: modify griditem style

--- src/components/styled_components/GridItem.js
+++ src/components/styled_components/GridItem.js
@@ -12,38 +12,45 @@ const StyledGridItem = styled(GridItem)`
   margin: 0.5rem 0;
   position: relative;
   border-radius: 10px;
-  border: 1px solid rgb(213, 213, 213);
+  /* border: 1px solid rgb(213, 213, 213); */
   box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
 
   h2 {
-    padding: 10px 10px;
+    padding: 10px 20px;
     font-family: 'Open Sans', sans-serif;
     font-size: 1.4rem;
     line-height: 1;
     text-align: left;
-    margin-top: 0;
+    margin: 20px 0 0px 0;
     color: #2e2e2e;
   }
 
   p {
-    padding: 0px 10px;
+    padding: 0px 20px;
     font-family: 'Montserrat', sans-serif;
-    font-size: 1rem;
+    font-size: 0.9rem;
     text-align: left;
-    /* margin: 0px 0 0 10px; */
+    line-height: 1.5;
+    margin: 0px 0px 20px 0px;
+    color: #2e2e2e;
   }
 
   ul {
     margin: 0;
     padding: 0;
+    display: flex;
   }
 
   li {
     list-style-type: none;
     background-color: #eee;
-    border-radius: 5px;
-    margin: 10px 0;
-    padding: 5px;
+    border-radius: 15px;
+    margin: 10px 5px;
+    padding: 8px;
+
+    &:hover {
+      cursor: default;
+    }
   }
 
   img {
