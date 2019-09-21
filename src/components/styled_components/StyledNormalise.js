62fd3aaf05e1697b152c079835004141c8d12407
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-21 16:55:54 +0530

    chore: modify GlobalStyles component

--- src/components/styled_components/StyledNormalise.js
+++ src/components/styled_components/StyledNormalise.js
@@ -7,13 +7,11 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    background-color: #f9fbff;
   }
 
   html, body {
 	height: 100%;
 	font-size: 1em;
-	background: #fafafa;
 	margin: auto;
 	box-sizing: border-box;
 }
