6219e48361c0a337a279f20ede0d307ce26e746a
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-21 16:53:50 +0530

    refactor: projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,17 +2,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Container from './styled_components/Container';
-import Projects from '../constants/projects';
-import StyledGridItem from './styled_components/GridItem';
-
-const GridContainer = styled.ul`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: repeat(auto-fill, 250px);
-  grid-template-rows: auto;
-  justify-content: space-around;
-  padding-inline-start: 0px;
-`;
+import GridItem from './styled_components/GridItem';
+import MainContainer from './styled_components/MainContainer';
+import GridContainer from './styled_components/GridContainer';
 
 const StyledHeading = styled.div`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:700|Montserrat=swap');
@@ -22,13 +14,17 @@ const StyledHeading = styled.div`
     font-family: 'Open Sans', sans-serif;
     font-size: 3rem;
     margin-bottom: 10px;
+    margin-top: 0;
+    padding: 50px 0 0 0;
     color: #2e2e2e;
   }
 
   p {
     font-family: 'Montserrat', sans-serif;
-    font-size: 1.8rem;
+    font-size: 1.5rem;
     margin-top: 5px;
+    margin-bottom: 30px;
+    color: #2e2e2e;
   }
 
   @media screen and (max-width: 599px) {
@@ -39,24 +35,21 @@ const StyledHeading = styled.div`
   }
 `;
 
-function Work({ className }) {
+function Work({ data }) {
   return (
-    <Container full fullVertical>
-      <StyledHeading style={{ textAlign: 'center' }}>
-        <h2>Projects</h2>
-        <p>Here are some of my recent projects.</p>
-      </StyledHeading>
-      <GridContainer className={className}>
-        {Projects.map((project, index) => (
-          <StyledGridItem
-            index={index}
-            project={project}
-            key={index}
-            style={{ listStyleType: 'none' }}
-          />
-        ))}
-      </GridContainer>
-    </Container>
+    <MainContainer bgColor="#f3f3f3">
+      <Container full backgroundColor="#f3f3f3" fullVertical>
+        <StyledHeading style={{ textAlign: 'center' }}>
+          <h2>Projects</h2>
+          <p>Here are some of my recent projects.</p>
+        </StyledHeading>
+        <GridContainer>
+          {data.map((project, index) => (
+            <GridItem index={index} project={project} key={index} style={{ listStyleType: 'none' }} />
+          ))}
+        </GridContainer>
+      </Container>
+    </MainContainer>
   );
 }
 
