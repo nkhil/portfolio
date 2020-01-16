eec021369286f86587d9656e80ed1fd13c15605c
Nikhil Vijayan <nkhilv@gmail.com>
2020-01-16 21:18:19 +0000

    :lipstick: make Hero elements responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import styled from 'styled-components';
+import Emoji from 'a11y-react-emoji';
 import HeroImage from './HeroImage';
 import Colours from '../constants/colours';
 import width from '../constants/width';
@@ -9,13 +10,15 @@ import width from '../constants/width';
 const Hero = ({ className }) => (
   <div className={className}>
     <div className="hero-container">
+      <HeroImage />
       <div className="hero-container-strap">
-        <h1>Hey, I'm Nikhil.</h1>
+        <h1>
+          Hey, I'm Nikhil <Emoji symbol="👋" label="wave" /> 
+        </h1>
         <div className="hero-container-strap-paragraph">
           <p>I’m a software developer who loves making things and solving interesting problems.</p>
         </div>
       </div>
-      <HeroImage />
     </div>
   </div>
 );
@@ -36,21 +39,43 @@ const StyledHero = styled(Hero)`
     .hero-container-strap {
       display: flex;
       flex-direction: column;
+      margin-left: 25px;
 
       .hero-container-strap-paragraph {
-        width: 350px;
+        width: 300px;
         p {
           font-size: 1.5rem;
           line-height: 1.3;
           color: ${Colours.bodyGrey};
-          margin: 5px;
+          margin: 5px 0;
         }
       }
 
       h1 {
         font-size: 4rem;
         color: ${Colours.headlineGrey};
-        margin: 5px;
+        margin: 0px;
+      }
+    }
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    .hero-container {
+      display: flex;
+      flex-direction: column;
+
+      .hero-container-strap {
+        margin-top: 20px;
+
+        h1 {
+          font-size: 3rem;
+          text-align: center;
+        }
+
+        p {
+          text-align: center;
+          font-size: 1.2rem;
+        }
       }
     }
   }
