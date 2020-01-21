cacd27cf1c13eba8d20a78b454347158e072b8bb
Nikhil Vijayan <nkhilv@gmail.com>
2020-01-21 22:19:07 +0000

    :recycle: abstract away blog container

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,71 +1,10 @@
 /* eslint-disable react/no-danger */
 /* eslint-disable react/prop-types */
-// /* eslint-disable react/prop-types */
-// /* eslint-disable react/no-danger */
 import React from 'react';
-import { graphql, Link } from 'gatsby';
-import styled from 'styled-components';
+import { graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
-
-const BlogContainer = styled.div`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:700|Montserrat=swap');
-  font-family: 'Open Sans', sans-serif;
-  margin: auto;
-  max-width: 650px;
-  display: flex;
-  flex-direction: column;
-  color: #282828;
-
-  h1 {
-    font-size: 3.5rem;
-  }
-
-  h2 {
-    font-size: 2rem;
-  }
-
-  h3 {
-    font-size: 1.5rem;
-  }
-
-  p {
-    font-family: 'Montserrat', sans-serif;
-    line-height: 1.8;
-    font-size: 1.1rem;
-    font-weight: inherit;
-    color: #282828;
-  }
-
-  code {
-    background: #f4f4f4;
-    border: 1px solid #ddd;
-    border-left: 3px solid #f36d33;
-    color: #666;
-    page-break-inside: avoid;
-    font-family: monospace;
-    font-size: 15px;
-    line-height: 1.6;
-    margin-bottom: 1.6em;
-    max-width: 100%;
-    overflow: auto;
-    padding: 1em 1.5em;
-    display: block;
-    word-wrap: break-word;
-  }
-
-  @media screen and (max-width: 599px) {
-    padding: 10px 15px;
-
-    h1 {
-      font-size: 3rem;
-    }
-
-    p {
-      font-size: 1.5rem;
-      margin-bottom: 50px;
-    }
-  }
-`;
+import PostContainer from '../components/PostContainer';
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -74,11 +13,14 @@ export default function Template({
 
   return (
     <>
+      <Helmet>
+        <title>{`Nikhil Vijayan - ${post.frontmatter.title}`}</title>
+      </Helmet>
       <Layout navbarWidth="750px" navlinkColor="#000" />
-      <BlogContainer>
+      <PostContainer>
         <h1>{post.frontmatter.title}</h1>
         <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
-      </BlogContainer>
+      </PostContainer>
     </>
   );
 }
