567dc39be6dfc20cb60823231efc9836cc995cf5
Nikhil Vijayan <nkhilv@gmail.com>
2020-01-21 22:19:24 +0000

    :readme2: modify projects constant

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -9,7 +9,7 @@ export const featuredProjects = [
   {
     title: 'Raspberry pi weather display',
     description: 'Laser-cut case to house a raspberry pi that fetches the weather and displays it on an e-ink display.',
-    path: '/project-title-here',
+    path: '/raspberry-pi-zero-weather-display',
     src: raspberry,
   },
   {
