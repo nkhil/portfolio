a44ad56974c8291c5188009fd6eada70e38dac9b
Nikhil <nikhil@jigsaw.xyz>
2020-06-09 21:57:21 +0100

    :lipstick: adjust logo padding

--- src/components/CssLogo/NewLogo.js
+++ src/components/CssLogo/NewLogo.js
@@ -18,7 +18,7 @@ const StyledLogo = styled(Logo)`
   padding-left: 50px;
   border-radius: 50%;
   background-color: black;
-  padding: 10px;
+  padding: 12px;
 
   a {
     text-decoration: none;
@@ -28,7 +28,7 @@ const StyledLogo = styled(Logo)`
   p {
     font-family: 'Inter', sans-serif;
     font-weight: 700;
-    font-size: 2.2rem;
+    font-size: 1.7rem;
     text-transform: uppercase;
     margin: 0;
     line-height: 0.8;
